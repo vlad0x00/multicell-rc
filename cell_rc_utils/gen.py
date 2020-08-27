b5810da2447918a8ef2e256f5bf7e2add7b7d1a6
schutzekatze <vladlich@protonmail.com>
2020-08-26 19:34:54 -0700

    Update

---
+++ cell_rc_utils/gen.py
@@ -0,0 +1,266 @@
+import os
+import sys
+import random
+import shutil
+import subprocess
+import numpy as np
+
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element, SubElement, Comment
+from xml.dom import minidom
+
+class Node:
+
+  def __init__(self, name):
+    self.name = name
+
+  def __str__(self):
+    return self.name
+
+  def __hash__(self):
+    return self.name.__hash__()
+
+def make_network_dot(num_genes, varf, dot_file):
+  import networkx as nx
+
+  graph = nx.DiGraph()
+
+  for cell_type, variable_matrix in enumerate(varf):
+    for gene, _ in enumerate(variable_matrix):
+      node = Node(str(cell_type) + "_" + str(gene))
+      graph.add_node(node)
+
+  for cell_type, variable_matrix in enumerate(varf):
+    for gene, variables in enumerate(variable_matrix):
+      node = Node(str(cell_type) + "_" + str(gene))
+      for v in variables:
+        var_node = Node(str(cell_type) + "_" + str(v))
+        graph.add_edge(var_node, node)
+
+  pydot_graph = nx.drawing.nx_pydot.to_pydot(graph)
+  pydot_graph.set_strict(False)
+  pydot_graph.set_name("gene_networks")
+  pydot_graph.write(dot_file, prog='dot')
+
+  if shutil.which('dot') is not None:
+    filename, file_extension = os.path.splitext(dot_file)
+    with open(filename + ".png", 'w') as f:
+      if num_genes > 100:
+        subprocess.run([ 'sfdp', '-x', '-Goverlap=scale', '-T', 'png', dot_file ], stdout=f)
+      else:
+        subprocess.run([ 'dot', '-T', 'png', dot_file ], stdout=f)
+
+def generate_gene_functions(num_cell_types, num_genes, connectivity, input_connections, num_cytokines, nv_file, varf_file, tt_file, dot_file):
+  assert input_connections < num_genes
+
+  nv = []
+  varf = []
+  tt = []
+
+  for cell_type in range(num_cell_types):
+    nv.append(np.zeros(num_genes, dtype=np.int32))
+
+    total_edges = (num_genes - 1) * connectivity - 2 * num_cytokines
+
+    possible_edges_no_input = (num_genes - 1) ** 2 - 2 * num_cytokines * (num_genes - 1) + num_cytokines ** 2
+    edges_no_input = [ (0, 0) ] * possible_edges_no_input
+    idx = 0
+    for i in range(1, num_genes):
+      for j in range(1, num_genes):
+        if j < (1 + num_cytokines): continue
+        if (1 + num_cytokines) <= i < (1 + 2 * num_cytokines): continue
+        edges_no_input[idx] = (i, j)
+        idx += 1
+    assert idx == possible_edges_no_input
+
+    possible_edges_input = num_genes - 1 - num_cytokines
+    edges_input = [ (0, 0) ] * possible_edges_input
+    idx = 0
+    for j in range(1 + num_cytokines, num_genes):
+      edges_input[idx] = (0, j)
+      idx += 1
+    assert idx == possible_edges_input
+
+    random.shuffle(edges_no_input)
+    random.shuffle(edges_input)
+
+    edges = edges_no_input[:(total_edges - input_connections)]
+    edges += edges_input[:input_connections]
+
+    for edge in edges:
+      nv[-1][edge[1]] += 1
+
+    assert (connectivity - 0.01) < (sum(nv[-1]) / (len(nv[-1]) - num_cytokines - 1)) < (connectivity + 0.01)
+
+    varf.append([])
+    for gene, n in enumerate(nv[-1]):
+      varf[-1].append([])
+      for _ in range(n):
+        idx = -1
+        for i, edge in enumerate(edges):
+          if edge[1] == gene:
+            varf[-1][-1].append(edge[0])
+            idx = i
+            break
+        edges.pop(idx)
+
+    tt.append([])
+    for gene, n in enumerate(nv[-1]):
+      if n > 0:
+        tt[-1].append(np.random.randint(2, size=(2 ** n)))
+      else:
+        tt[-1].append([])
+
+  with open(nv_file, 'w') as f:
+    for cell_type_nv in nv:
+      f.write(' '.join([str(x) for x in cell_type_nv]))
+      f.write('\n')
+
+  with open(varf_file, 'w') as f:
+    for cell_type_varf in varf:
+      for gene_varf in cell_type_varf:
+        f.write(' '.join([str(x) for x in gene_varf]))
+        f.write('\n')
+
+  with open(tt_file, 'w') as f:
+    for cell_type_tt in tt:
+      for gene_tt in cell_type_tt:
+        f.write(' '.join([str(x) for x in gene_tt]))
+        f.write('\n')
+
+  if dot_file != None:
+    make_network_dot(num_genes, varf, dot_file)
+
+def generate_input_signal(signal_len, signal_file):
+  arr = np.random.randint(2, size=signal_len)
+  arr[0] = 0 # Initial substance level is 0, otherwise grid phi initialization is non-trivial
+  with open(signal_file, 'w') as f:
+    f.write(' '.join([str(x) for x in arr]))
+
+def generate_gene_initial_states(num_genes, num_cells, num_cytokines, input_signal_file, state_file):
+  with open(input_signal_file, 'r') as f:
+    input_signal = [ int(x) for x in f.readline().split() ]
+  with open(state_file, 'w') as f:
+    for _ in range(num_cells):
+      state = np.random.randint(2, size=num_genes)
+      state[0] = input_signal[0] # 0
+      for i in range(num_cytokines):
+        state[i + 1] = 0 # Initial substance levels are 0, otherwise grid phi initialization is non-trivial
+      f.write(' '.join([str(x) for x in state]))
+      f.write('\n')
+
+def prettify(elem):
+  """Return a pretty-printed XML string for the Element.
+  """
+  rough_string = ET.tostring(elem, 'utf-8')
+  reparsed = minidom.parseString(rough_string)
+  return reparsed.toprettyxml(indent="  ")
+
+def make_params_xml(xml_path, output_dir, simulation_steps, additional_params, threads, universe_length, universe_width):
+  xml_file = open(xml_path, 'w')
+
+  # Biocellion required parameters
+  bcell_num_baseline = simulation_steps
+  bcell_nx = str(max(4, universe_width))
+  bcell_ny = str(max(4, universe_width))
+  bcell_nz = str(max(4, universe_length))
+  bcell_partition_size = max(4, universe_width, universe_length)
+  bcell_path = output_dir
+  bcell_interval = 1
+  bcell_start_x = 0
+  bcell_start_y = 0
+  bcell_start_z = 0
+  bcell_size_x = max(4, universe_width)
+  bcell_size_y = max(4, universe_width)
+  bcell_size_z = max(4, universe_length)
+
+  # Biocellion optional parameteres
+  bcell_input_param = additional_params
+  bcell_verbosity = 0 # [0-5]
+
+  bcell_num_threads = threads
+  bcell_num_node_groups = 1
+  bcell_num_nodes_per_group = 1
+  bcell_num_sockets_per_node = 1
+  bcell_max_load_imbalance = 1.2
+
+  supersize = max(bcell_size_x, bcell_size_y, bcell_size_z)
+  bcell_super_x = supersize
+  bcell_super_y = supersize
+  bcell_super_z = supersize
+
+  bcell_summary = 1
+  bcell_load_balance = 0
+  bcell_regridding = 1000
+  bcell_checkpoint = 0
+
+  bcell_refine_ratio = 2
+  bcell_fill_ratio = 0.5
+
+  #  Write xml for Biocellion
+  top = Element('biocellion')
+
+  comment = Comment('Input parameters for Biocellion, generated by Python parser.')
+  top.append(comment)
+
+  # Printing required parameters
+  xml_required = SubElement(top, 'required')
+  xml_required.append(Comment('Required Parameters for biocellion.'))
+
+  xml_time_step = SubElement(xml_required, 'time_step')
+  xml_time_step.set('num_baseline', str(bcell_num_baseline))
+
+  xml_domain = SubElement(xml_required, 'domain')
+  xml_domain.set('x', str(bcell_nx))
+  xml_domain.set('y', str(bcell_ny))
+  xml_domain.set('z', str(bcell_nz))
+
+  xml_init_data = SubElement(xml_required, 'init_data')
+  xml_init_data.set('partition_size', str(bcell_partition_size))
+  xml_init_data.set('src', 'code')
+
+  xml_output = SubElement(xml_required, 'output ')
+  xml_output.set('path', bcell_path)
+  xml_output.set('interval', str(bcell_interval))
+  xml_output.set('particle', 'pvtp')
+  xml_output.set('grid', 'vthb')
+  xml_output.set('start_x', str(bcell_start_x))
+  xml_output.set('start_y', str(bcell_start_y))
+  xml_output.set('start_z', str(bcell_start_z))
+  xml_output.set('size_x', str(bcell_size_x))
+  xml_output.set('size_y', str(bcell_size_y))
+  xml_output.set('size_z', str(bcell_size_z))
+
+  # Printing Required parameterss
+  xml_optional = SubElement(top, 'optional')
+  xml_optional.append(Comment('Optional parameters for biocellion.'))
+
+  xml_bioparm_model = SubElement(xml_optional, 'model')
+  xml_bioparm_model.set('param', bcell_input_param)
+
+  xml_bioparm_stdout = SubElement(xml_optional, 'stdout')
+  xml_bioparm_stdout.set('verbosity', str(bcell_verbosity))
+
+  xml_bioparm_system = SubElement(xml_optional, 'system')
+  xml_bioparm_system.set('num_node_groups', str(bcell_num_node_groups))
+  xml_bioparm_system.set('num_nodes_per_group', str(bcell_num_nodes_per_group))
+  xml_bioparm_system.set('num_sockets_per_node', str(bcell_num_sockets_per_node))
+  xml_bioparm_system.set('max_load_imbalance', str(bcell_max_load_imbalance))#
+  xml_bioparm_system.set('num_threads', str(bcell_num_threads))
+
+  xml_bioparm_super_partition = SubElement(xml_optional, 'super_partition')
+  xml_bioparm_super_partition.set('x', str(bcell_super_x))
+  xml_bioparm_super_partition.set('y', str(bcell_super_y))
+  xml_bioparm_super_partition.set('z', str(bcell_super_z))
+
+  xml_interval = SubElement(xml_optional, 'interval')
+  xml_interval.set('summary', str(bcell_summary))
+  xml_interval.set('load_balance', str(bcell_load_balance))
+  xml_interval.set('regridding', str(bcell_regridding))
+  xml_interval.set('checkpoint', str(bcell_checkpoint))
+
+  xml_amr = SubElement(xml_optional, 'amr')
+  xml_amr.set('refine_ratio', str(bcell_refine_ratio))
+  xml_amr.set('fill_ratio', str(bcell_fill_ratio))
+
+  xml_file.write(prettify(top))
---
+++ cell_rc_utils/train.py
@@ -0,0 +1,241 @@
+import os
+import sys
+import shutil
+import subprocess
+import struct
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+from vtk import vtkXMLPolyDataReader
+from vtk.util.numpy_support import vtk_to_numpy
+
+STATES_FILE = 'states'
+
+def get_gene_values(output_file, num_genes, num_cells):
+  reader = vtkXMLPolyDataReader()
+  reader.SetFileName(output_file)
+  reader.Update()
+  data = reader.GetOutput()
+
+  assert data.GetNumberOfPoints() == num_cells
+
+  cell_ids = [ 0 ] * num_cells
+  for cell in range(num_cells):
+    cell_ids[cell] = int(data.GetPointData().GetArray(1).GetTuple(cell)[0])
+
+  gene_values = [ 0 ] * (num_cells * num_genes)
+  for cell in range(num_cells):
+    cell_id = cell_ids[cell]
+    gene_count = 0
+    for g in range(2, data.GetPointData().GetNumberOfArrays()):
+      bits = data.GetPointData().GetArray(g).GetTuple(cell)[0]
+      bits = struct.unpack('Q', struct.pack('d', bits))[0]
+      width = min(num_genes - gene_count, 64)
+      offset = cell_id * num_genes + gene_count
+      vals = [ int(n) for n in bin(bits)[2:].zfill(width) ]
+      gene_values[(offset):(offset + width)] = vals[0:width]
+      gene_count += width
+
+  return gene_values
+
+def get_states(num_genes, num_output_genes, num_cells, window_size, timesteps, output_dir, dump):
+  states = [ [] for _ in range(timesteps + 1) ]
+  for step in range(timesteps + 1):
+    states[step] = get_gene_values(os.path.join(output_dir, 'agent_0_0_0_' + str(step) + '.vtp'), num_genes, num_cells)
+
+  if dump:
+    with open(os.path.join(output_dir, STATES_FILE), 'w') as f:
+      for state in states:
+        for i, bit in enumerate(state):
+          if i > 0:
+            f.write(' ')
+          f.write(str(bit))
+        f.write('\n')
+
+  return states
+
+def get_cell_types(output_file):
+  cell_type_map = {}
+  with open(output_file, 'r') as f:
+    for line in f:
+      if line.startswith("Cell:"):
+        tokens = line.split(',')
+        cell = int(tokens[0].split(':')[1])
+        cell_type = int(tokens[1].split(':')[1])
+        cell_type_map[cell] = cell_type
+  return cell_type_map
+
+def process_output(input_signal_file, biocellion_output_file, output_dir, num_genes, num_cells, num_output_genes, num_output_cells, num_output_cell_types, window_size, delay, timesteps, function, auxiliary_files, threads, warmup_steps, tissue_depth):
+  with open(input_signal_file) as f:
+    input_signal = [ int(x) for x in f.readline().split() ]
+
+  states = get_states(num_genes, num_output_genes, num_cells, window_size, timesteps, output_dir, auxiliary_files)
+
+  cell_input_matches = [ [] for _ in range(num_cells) ]
+  cell_input_constant = [ [] for _ in range(num_cells) ]
+  for cell in range(num_cells):
+    cell_input_matches[cell] = [ False ] * len(input_signal)
+    cell_input_constant[cell] = [ False ] * len(input_signal)
+    for i, (signal, genes) in enumerate(zip(input_signal, states)):
+      cell_input_matches[cell][i] = (signal == genes[cell * num_genes])
+      cell_input_constant[cell][i] = (states[0][cell * num_genes] == genes[cell * num_genes])
+  input_signal_info = {}
+  for layer in range(tissue_depth):
+    input_signal_info[layer] = { "correct_cells" : 0, "bad_cells" : 0, "total_cells" : 0 }
+
+  z_layers = tissue_depth
+  y_layers = math.ceil(math.sqrt(num_cells / z_layers))
+  x_layers = math.ceil(num_cells / z_layers / y_layers)
+  assert x_layers * y_layers * z_layers >= num_cells
+
+  for cell, (cell_matches, cell_constant) in enumerate(zip(cell_input_matches, cell_input_constant)):
+    layer = cell // (x_layers * y_layers)
+    if all(cell_matches):
+      input_signal_info[layer]["correct_cells"] += 1
+    elif not all(cell_constant):
+      input_signal_info[layer]["bad_cells"] += 1
+    input_signal_info[layer]["total_cells"] += 1
+  assert input_signal_info[0]["correct_cells"] > 0
+  cells_correct_input = 0
+  cells_bad_input = 0
+  for layer in range(tissue_depth):
+    cells_correct_input += input_signal_info[layer]["correct_cells"]
+    cells_bad_input += input_signal_info[layer]["bad_cells"]
+  input_signal_info["correct_input"] = cells_correct_input
+  input_signal_info["bad_input"] = cells_bad_input
+
+  states = states[(window_size - 1):]
+  for state in states:
+    assert len(state) == num_cells * num_genes
+
+  cell_types_map = get_cell_types(biocellion_output_file)
+
+  output_cells = range(num_cells - 1, num_cells - num_output_cells - 1, -1)
+  output_states = [ [] for _ in range(len(states)) ]
+  for i, state in enumerate(states):
+    for cell in output_cells:
+      cell_type = cell_types_map[cell]
+      if cell_type >= num_output_cell_types: continue
+      cell_state = state[(cell * num_genes):((cell + 1) * num_genes)]
+      assert len(cell_state) == num_genes
+      output_states[i] += cell_state[-num_output_genes:]
+  for state in output_states:
+    assert len(state) == len(output_states[0])
+  if all([ cell_type >= num_output_cell_types for cell_type in [ cell_types_map[cell] for cell in output_cells ] ]):
+    print("\nNone of the output cells belong to the output cell type. Exiting...")
+    sys.exit(1)
+
+  x = output_states
+  if function == 'parity':
+    parity = [ 0 ] * (len(input_signal) + 1 - window_size)
+    for i, j in enumerate(range(window_size, len(input_signal) + 1)):
+      bitsum = 0
+      for bit in input_signal[(j - window_size):j]:
+        bitsum += bit
+      if bitsum % 2 == 0:
+        parity[i] = 0
+      else:
+        parity[i] = 1
+    assert len(input_signal) == len(parity) + window_size - 1
+    y = parity
+  elif function == 'median':
+    median = [ 0 ] * (len(input_signal) + 1 - window_size)
+    for i, j in enumerate(range(window_size, len(input_signal) + 1)):
+      bitsum = 0
+      for bit in input_signal[(j - window_size):j]:
+        bitsum += bit
+      if bitsum > window_size // 2:
+        median[i] = 1
+      else:
+        median[i] = 0
+    assert len(input_signal) == len(median) + window_size - 1
+    y = median
+
+  if delay > 0:
+    x = x[delay:]
+    y = y[:-delay]
+
+  if warmup_steps > 0:
+    x = x[warmup_steps:]
+    y = y[warmup_steps:]
+
+  assert len(x) == len(y)
+  for s in x:
+    assert len(s) <= num_output_cells * num_output_genes
+
+  return x, y, input_signal_info
+
+def train_lasso(x, y, num_cells, num_output_cells, num_output_cell_types, num_output_genes, biocellion_output_file, output_dir, threads):
+  from sklearn.linear_model import Lasso, LassoCV
+  from sklearn.model_selection import train_test_split
+  from sklearn.utils import parallel_backend
+
+  x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+
+  lasso = LassoCV(n_jobs=threads, selection='random', tol=0.05)
+  lasso.fit(x_train, y_train)
+
+  train_predicted = [ 1 if x > 0.5 else 0 for x in lasso.predict(x_train) ]
+  test_predicted = [ 1 if x > 0.5 else 0 for x in lasso.predict(x_test) ]
+
+  train_score = lasso.score(x_train, y_train)
+  test_score = lasso.score(x_test, y_test)
+  coeff_used = np.sum(lasso.coef_ != 0)
+  coeffs = len(x[0])
+
+  train_accuracy = sum([ 1 if a == b else 0 for a, b in zip(train_predicted, y_train) ]) / len(train_predicted)
+  test_accuracy = sum([ 1 if a == b else 0 for a, b in zip(test_predicted, y_test) ]) / len(test_predicted)
+
+  cell_types_map = get_cell_types(biocellion_output_file)
+  cell_feature_count = {}
+  cell_type_feature_count = {}
+  for cell in range(num_cells - 1, num_cells - num_output_cells - 1, -1):
+    cell_type = cell_types_map[cell]
+    if cell_type >= num_output_cell_types: continue
+    coeff_start = (num_cells - 1 - cell) * num_output_genes
+    coeff_end = (num_cells - cell) * num_output_genes
+    feature_count = np.sum(lasso.coef_[coeff_start:coeff_end] != 0)
+    if feature_count > 0:
+      if cell in cell_feature_count:
+        cell_feature_count[cell] += feature_count
+      else:
+        cell_feature_count[cell] = feature_count
+      if cell_type in cell_type_feature_count:
+        cell_type_feature_count[cell_type] += feature_count
+      else:
+        cell_type_feature_count[cell_type] = feature_count
+  
+  plt.figure()
+  plt.bar(range(len(cell_feature_count)), sorted(cell_feature_count.values(), reverse=True))
+  xticks = [math.ceil(x) for x in plt.xticks()[0]]
+  xticks_new = []
+  for xtick in xticks:
+    if xtick < 0: continue
+    if xtick >= len(cell_feature_count):
+      xticks_new.append(len(cell_feature_count) - 1)
+      break
+    else:
+      xticks_new.append(xtick)
+  xticks = xticks_new
+  plt.xticks(xticks)
+  plt.yticks([math.ceil(y) for y in plt.yticks()[0]])
+  plt.savefig(os.path.join(output_dir, "cell_feature_count.png"))
+
+  plt.figure()
+  plt.bar(range(len(cell_type_feature_count)), sorted(cell_type_feature_count.values(), reverse=True))
+  xticks = [math.ceil(x) for x in plt.xticks()[0]]
+  xticks_new = []
+  for xtick in xticks:
+    if xtick < 0: continue
+    if xtick >= len(cell_type_feature_count):
+      xticks_new.append(len(cell_type_feature_count) - 1)
+      break
+    else:
+      xticks_new.append(xtick)
+  xticks = xticks_new
+  plt.xticks(xticks)
+  plt.yticks([math.ceil(y) for y in plt.yticks()[0]])
+  plt.savefig(os.path.join(output_dir, "cell_type_feature_count.png"))
+
+  return train_accuracy, test_accuracy, coeff_used, coeffs
