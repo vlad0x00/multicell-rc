c63f427b36c30e882e4059ea63511eb6984b35fa
schutzekatze <vladlich@protonmail.com>
2020-09-25 02:07:16 -0700

    Update

--- multicell_rc_utils/plot_single_param.R
+++ multicell_rc_utils/plot_single_param.R
@@ -12,15 +12,19 @@ results <- results[, -1]
 results$Secretion.high <- factor(results$Secretion.high)
 results$Cytokines <- factor(results$Cytokines)
 
-results$Cells <- floor(results$Cells / 10)
+results$Genes <- floor(results$Genes / 5) * 5
+results$Cells <- floor(results$Cells / 10) * 10
+
+# The code below adds a suffix 'cells' to the cells column rows
 #cells <- paste(results$Cells, 'cells')
 #cells_levels <- paste(unique(sort(results$Cells)), 'cells')
 #results$Cells <- factor(cells, levels = cells_levels)
 
-results %>% ggplot(aes(x=Cytokines, y=Accuracy, group=1)) +
-  stat_summary(geom="ribbon", fun.data=mean_cl_normal,fun.args=list(conf.int=0.95), fill="lightblue", alpha=0.5) +
+results %>% ggplot(aes(x=Cell.types, y=Accuracy, group=1)) +
+  #stat_summary(geom="ribbon", fun.data=mean_cl_normal,fun.args=list(conf.int=0.95), fill="lightblue", alpha=0.5) +
   stat_summary(geom="line", fun=mean, linetype="dashed") +
   stat_summary(geom="point", fun=mean, color="red") +
+  geom_smooth(formula=y ~ x, method=stats::loess) +
   theme_bw() +
-  theme(axis.title.y = element_text(angle = 0))
+  theme(text = element_text(size = 26), axis.title.y = element_text(angle = 0))
 ggsave('plot.png', width = 20, height = 15)
