224a0f1298ae1d2b2f570f253b315fde89f612a5
schutzekatze <vladlich@protonmail.com>
2020-10-30 00:58:55 -0700

    Update

--- multicell_rc_utils/plot_heatmap.R
+++ multicell_rc_utils/plot_heatmap.R
@@ -6,32 +6,38 @@ library(dplyr)
 library(grid)
 library(gridExtra)
 library(stringr)
+library(viridis)
+library(viridisLite)
 
-results_2layers <- read.csv('results_2layers.csv')
-rownames(results_2layers) <- results_2layers[, 1]
-results_2layers <- results_2layers[, -1]
+#results_2layers <- read.csv('results_2layers.csv')
+#rownames(results_2layers) <- results_2layers[, 1]
+#results_2layers <- results_2layers[, -1]
 
-results_3layers <- read.csv('results_3layers.csv')
-rownames(results_3layers) <- results_3layers[, 1]
-results_3layers <- results_3layers[, -1]
+#results_3layers <- read.csv('results_3layers.csv')
+#rownames(results_3layers) <- results_3layers[, 1]
+#results_3layers <- results_3layers[, -1]
 
-results_4layers <- read.csv('results_4layers.csv')
-rownames(results_4layers) <- results_4layers[, 1]
-results_4layers <- results_4layers[, -1]
+#results_4layers <- read.csv('results_4layers.csv')
+#rownames(results_4layers) <- results_4layers[, 1]
+#results_4layers <- results_4layers[, -1]
 
-results_5layers <- read.csv('results_5layers.csv')
-rownames(results_5layers) <- results_5layers[, 1]
-results_5layers <- results_5layers[, -1]
+#results_5layers <- read.csv('results_5layers.csv')
+#rownames(results_5layers) <- results_5layers[, 1]
+#results_5layers <- results_5layers[, -1]
 
-results <- rbind(results_2layers, results_3layers, results_4layers, results_5layers)
+#results <- rbind(results_2layers, results_3layers, results_4layers, results_5layers)
+
+results <- read.csv('results.csv')
+rownames(results) <- results[, 1]
+results <- results[, -1]
 
 results <- select(results, c(Accuracy, Cell.types, Cytokines, Tissue.depth))
 results <- aggregate(results, by = list(results$Cell.types, results$Cytokines, results$Tissue.depth), FUN = mean)
 
 results %>% ggplot(aes(x=Cell.types, y=Cytokines, fill=Accuracy)) +
-  scale_fill_continuous(type = "inferno") +
+  scale_fill_viridis(option = "inferno") +
   geom_tile() +
   theme_bw() +
-  facet_wrap(~Tissue.depth) +
+#  facet_wrap(~Tissue.depth) +
   theme(text = element_text(size = 26), axis.title.y = element_text(angle = 0))
 ggsave('plot.png', width = 20, height = 15)
