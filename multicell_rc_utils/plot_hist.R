b9e5b60c53cb9a6045dc39f1df6bd8abebc81375
schutzekatze <vladlich@protonmail.com>
2021-01-22 00:45:02 -0800

    Update

--- multicell_rc_utils/plot_hist.R
+++ multicell_rc_utils/plot_hist.R
@@ -15,12 +15,8 @@ if (length(args) != 1) {
 
 results <- read.csv('results.csv')
 
-results$Tissue.depth <- factor(results$Tissue.depth)
-
-ggplot(results, aes_string(x=args[1], y="Accuracy", color="Tissue.depth")) +
-  stat_summary(geom="line", fun=mean, linetype="dashed") +
-  #stat_summary(geom="point", fun=mean, color="red") +
-  geom_smooth(formula=y ~ x, method=stats::loess) +
+ggplot(results, aes_string(x=args[1])) +
+  geom_histogram() +
   theme_bw() +
   theme(text = element_text(size = 26), axis.title.y = element_text(angle = 0))
 ggsave('plot.png', width = 20, height = 15)
